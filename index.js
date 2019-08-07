485a587acaa0885ad4f30e42f8549d8bd929cbd0
Pav0l <pavol.karas@gmail.com>
2019-08-07 19:05:45 +0200

    Add sanity check endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require('./api/index');
 
 const PORT = process.env.PORT || 9000;
 
+server.get('/', (req, res) => {
+  res.status(200).json('Server is alive!');
+});
+
 server.listen(PORT, () => {
   console.log(
     `*** Server listening on port: ${PORT} in ${server.settings.env} mode ***`
