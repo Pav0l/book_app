43c195146bbe0fff61f673f2b2de2779733b24a5
Pav0l <pavol.karas@gmail.com>
2019-08-07 21:48:15 +0200

    Connect books and authors routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,11 @@
 const path = require('../../util/paths');
+const authorsRouter = require('./authors');
+const booksRouter = require('./books');
 
 module.exports = server => {
+  server.use(path.authors, authorsRouter);
+  server.use(path.books, booksRouter);
+
   server.get('/', (req, res) => {
     res.status(200).json('Server is alive!');
   });
