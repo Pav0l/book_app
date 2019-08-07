027e7854572f5bf7e559e15e2bc5d81280fc4cee
Pav0l <pavol.karas@gmail.com>
2019-08-07 19:03:28 +0200

    Start listening for requests

---
+++ index.js
@@ -0,0 +1,9 @@
+const server = require('./api/index');
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+  console.log(
+    `*** Server listening on port: ${PORT} in ${server.settings.env} mode ***`
+  );
+});
